2987a89b57e1c47dede0a83f44d0ceebb3fc91ac
ElalmiBoudjema <70419216+ElalmiBoudjema@users.noreply.github.com>
2020-10-10 16:27:48 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            for (int i = 0; i <= 5; i++)
+
+            {
+               
+
+                    
+                    
+                
+                    
+                    Console.WriteLine("\n\t multiple {0}",i);
+                if (i > 2)
+                    continue;
+
+                for (int j = 0; j <= 10; j++)
+                {
+                    
+                        
+
+                    Console.WriteLine("\n\t\t{0}/{1}={2}",i,j,i*j);
+                   
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
